f91a49b40b33da92da5afc8589404fd96f17ab60
dawei-wang <dawei-wang@users.noreply.github.com>
2023-07-29 20:35:02 -0700

    Update UI

--- Street-Smart.py
+++ Street-Smart.py
@@ -44,30 +44,54 @@ st.title("Street Crossing Game")
 st.metric("Points", 0)
 st.metric("Streets Crossed", 0)
 
+# Initialize points and streets
+if "points" not in st.session_state:
+    st.session_state["points"] = 0
+
+if "streets" not in st.session_state:
+    st.session_state["streets"] = 0
+
 col1, col2 = st.columns(2)
 
 with col1:
     if st.button("Cross Street"):
         st.session_state["points"] += 10
         st.session_state["streets"] += 1
-        elevenlabs.generate("You crossed the street!", voice=selected_voice)
+        elevenlabs.play(
+            elevenlabs.generate("You crossed the street!",
+                                voice=selected_voice)
+        )
 
 with col2:
     if st.button("Answer Question"):
         q = random.choice(questions)
-        elevenlabs.generate(q["question"], voice=selected_voice)
+        # Speak and display question
+        elevenlabs.play(elevenlabs.generate(
+            q["question"], voice=selected_voice))
+        st.write(q["question"])
 
-        user_answer = st.radio(
+        # Replay audio
+        replay = st.button("Replay Audio")
+        if replay:
+            elevenlabs.play(elevenlabs.generate(q["question"]))
+
+    # Get user selection
+        selection = st.radio(
             "Select your answer:",
             q["choices"]
         )
-        elevenlabs.generate(
-            f"You selected: {user_answer}", voice=selected_voice)
 
-        if user_answer == q["answer"]:
+    # Read back selection
+        elevenlabs.play(elevenlabs.generate(f"You selected: {selection}"))
+
+    # Submit answer
+    submit = st.button("Submit Answer")
+    if submit:
+
+        if selection == q["answer"]:
             st.success("Correct!")
             st.session_state["points"] += 10
-        else:
-            st.error(f"Incorrect, the answer is {q['answer']}")
 
+        else:
+            st.error("Incorrect!")
 st.experimental_rerun()
